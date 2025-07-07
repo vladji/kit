a79a5430caa324cbbaca82c86fe837c1cd163d9a
vladji <vladjislav@gmail.com>
2025-07-07 15:59:14 +0700

    init project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { BadgeDollarSign } from "lucide-react-native";
+import { StatusBar, StyleSheet, useColorScheme, View } from "react-native";
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      <View>
+        <BadgeDollarSign />
+      </View>
     </View>
   );
 }
