a9f64ac301a8e50ba7be6248282b643d21240b5c
vladji <vladjislav@gmail.com>
2025-07-19 11:51:02 +0700

    feat: connect socket

--- src/app/api/constants.ts
+++ src/app/api/constants.ts
@@ -1,6 +1,9 @@
-import { REACT_API_URL } from '@env';
+import { REACT_SERVER_URL } from '@env';
 
-export const API_URL = __DEV__ ? 'http://172.20.10.3:3001/api' : REACT_API_URL;
+export const SERVER_URL = __DEV__
+  ? 'http://192.168.1.36:3001'
+  : REACT_SERVER_URL;
+export const API_URL = `${SERVER_URL}/api`;
 
 export const QUERY_KEYS = {
   GET_MESSAGES: 'get_messages',
---
+++ src/entities/Chat/lib/socket.ts
@@ -0,0 +1,26 @@
+import { Socket, io } from 'socket.io-client';
+import { SERVER_URL } from 'app/api/constants.ts';
+
+const URL = SERVER_URL;
+
+let socket: Socket;
+
+export const connectSocket = (userId: string) => {
+  console.log('connectSocket', { userId });
+
+  socket = io(URL, {
+    transports: ['websocket'],
+    reconnection: true,
+  });
+
+  socket.on('connect', () => {
+    console.log('✅ Connected to socket server');
+    socket.emit('register', userId);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('❌ Disconnected from socket server');
+  });
+};
+
+export const getSocket = () => socket;
